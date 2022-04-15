32976e09f5995c86f02d3b97049b66fd6d82bea2
이성영 <syounglee012@gmail.com>
2022-04-15 10:15:49 -0700

    4/16 SAT 2:15AM

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,120 @@
 import React from "react";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import post from "../redux/modules/post";
+import { useEffect } from "react";
+import { actionCreators as postActions } from "../redux/modules/post";
+
 
 const Main = (props) => {
-  return <React.Fragment></React.Fragment>;
+  const dispatch = useDispatch();
+  const post_list = useSelector((state) => state.post.list);
+  console.log(post_list);
+  
+  useEffect(() => {
+    dispatch(postActions.getPostDB());
+  },[]);
+
+  return (
+    <React.Fragment>
+      <Responsive>
+        <Bigbox>
+          {post_list.map((post) => (
+            <Midbox key={post._Id}>
+              <Box > {post.image}</Box>
+              <Box1>
+                {" "}
+                {post.title}
+                {post.content}
+                {post.comment}
+                {post.date}
+              </Box1>
+              <Box2> {post.userName} </Box2>
+            </Midbox>
+          ))}
+        </Bigbox>
+      </Responsive>
+    </React.Fragment>
+  );
 };
 
 export default Main;
+
+const Box = styled.div`
+  width: 350px;
+  height: 200px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  border: 2px solid black;
+  margin: 5px 5px;
+`;
+const Box1 = styled.div`
+  width: 350px;
+  height: 120px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  border: 2px solid black;
+  margin: 1px 5px;
+`;
+const Box2 = styled.div`
+  width: 350px;
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  border: 2px solid black;
+  margin: 2px 5px;
+`;
+
+const Midbox = styled.div`
+  width: 350px;
+  height: 400px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  border: 2px solid red;
+  margin: 10px 10px;
+`;
+
+const Bigbox = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  border: 2px solid blue;
+  margin-top: 50px;
+`;
+
+const Responsive = styled.div`
+  /* ${(prop) => prop.theme.responsiveContainer}; */
+  max-width: 100vw;
+  border: 2px solid black;
+  height: 100;
+`;
+
+const TabContainer = styled.div`
+  display: flex;
+  width: 93%;
+  margin: auto;
+  flex-direction: column;
+  height: 100%;
+  margin-top: 2rem;
+  border: 2px solid red;
+`;
+
+const ListContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  border: 2px solid blue;
+`;
--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,7 +1,288 @@
 import React from "react";
+import styled from "styled-components";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { actionCreators as postActions } from "../redux/modules/post";
+import { actionCreators as imageActions } from "../redux/modules/image";
 
 const Write = (props) => {
-  return <React.Fragment>하이</React.Fragment>;
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const fileInput = React.useRef(null);
+  const preview = useSelector((state) => state.image.preview);
+  const posts = useSelector((state) => state.post.list);
+  const [post, setPost] = useState({});
+
+  const postId = props.match.params.id;
+  const is_edit = postId ? true : false;
+  let _post = is_edit ? posts.find((p) => p.id === postId) : null;
+
+  const selectFile = (e) => {
+    console.log(e.target.files);
+
+    const reader = new FileReader();
+    const file = fileInput.current.files[0];
+
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      dispatch(imageActions.setPreview(reader.result));
+    };
+  };
+
+  const is_uploading = useSelector((state) => state.image.uploading);
+  // computed property names 문법 (키값 동적 할당)
+  const handleForm = (e) => {
+    setPost({
+      ...post,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //  폼데이터 콘솔 찍기
+  //   for (var pair of formData.entries()) {
+  // }
+  const createPost = () => {
+    if (post.title === "" || post.content === "") {
+      window.alert("내용을 추가 해주세요");
+      return;
+    }
+    if (fileInput === null) {
+      window.alert("이미지를 추가 해주세요");
+    }
+    const formData = new FormData();
+    if (fileInput.current) {
+      formData.append("title", post.title);
+      formData.append("content", post.content);
+      formData.append("image", fileInput.current.files[0]);
+    }
+    console.log(formData);
+    dispatch(postActions.addPostDB(formData));
+    history.push("/");
+  };
+
+  const editPost = () => {
+    const file = fileInput.current.files[0];
+
+    const formData = new FormData();
+
+    formData.append("title", post.title);
+    formData.append("image", file);
+    formData.append("content", post.content);
+    return dispatch(postActions.editPostDB(postId, formData));
+  };
+
+  return (
+    <React.Fragment>
+      <Container>
+        <Wrapper>
+          <Title
+            name="title"
+            value={post.title}
+            placeholder="제목을 입력하세요"
+            onChange={handleForm}
+          ></Title>
+          <Line />
+
+          <HashTagWrapper>
+            <Input1 type="text" placeholder="태그를 입력하세요" />
+            <AddImage
+              disabled={is_uploading}
+              type="file"
+              ref={fileInput}
+              onChange={selectFile}
+            />
+            <AspectOutter>
+              <AspectInner src={preview ? preview : null}></AspectInner>
+            </AspectOutter>
+
+            <Input2
+              name="content"
+              value={post.content}
+              type="text"
+              placeholder="당신의 이야기를 적어보세요..."
+              onChange={handleForm}
+            />
+          </HashTagWrapper>
+        </Wrapper>
+        {/* editor는 토스트 ui사용 */}
+      </Container>
+      <Footer>
+        <button
+          className="exit"
+          onClick={() => {
+            props.history.push("/");
+          }}
+        >
+          <span>나가기</span>
+        </button>
+        <Buttons>
+          <button className="cancle" onClick={editPost}>
+            임시저장
+          </button>
+          <button className="submit" onClick={createPost}>
+            출간하기
+          </button>
+        </Buttons>
+      </Footer>
+    </React.Fragment>
+  );
 };
 
 export default Write;
+
+const Container = styled.div`
+  ${(props) => props.theme.border_box};
+  padding: 0 1rem;
+  width: 100vw;
+`;
+
+const Footer = styled.div`
+  ${(props) => props.theme.border_box};
+  padding-left: 1rem;
+  padding-right: 1rem;
+  height: 4rem;
+  width: 100vw;
+  box-shadow: rgb(0 0 0 / 10%) 0px 0px 8px;
+  background: rgba(255, 255, 255, 0.85);
+  display: flex;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  background-color: white;
+  & button.exit {
+    height: 2.5rem;
+    padding: 0.5rem 1rem;
+    -webkit-box-align: center;
+    display: flex;
+    align-items: center;
+    background: none;
+    border-radius: 4px;
+    cursor: pointer;
+    border: none;
+    outline: none;
+    font-size: 1.125rem;
+    &:hover {
+      background-color: rgb(233, 236, 239);
+    }
+    & span {
+      margin-left: 0.5rem;
+      font-weight: 450;
+    }
+  }
+`;
+const Tag = styled.div`
+  ${(props) => props.theme.flex_row};
+  color: ${(props) => props.theme.velog_green};
+  background-color: ${(props) => props.theme.post_bg};
+  padding: 0 0.75rem;
+  height: 1.5rem;
+  border-radius: 0.75rem;
+  font-size: 12px;
+  margin: 0.5rem 0;
+  cursor: pointer;
+  margin-right: 0.5rem;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  -webkit-box-pack: end;
+  justify-content: flex-end;
+  & button {
+    cursor: pointer;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    padding: 0px 1.25rem;
+    height: 2.5rem;
+    font-size: 1.125rem;
+    font-weight: bold;
+    outline: none;
+  }
+  & button.cancle {
+    margin-right: 0.75rem;
+    background: rgb(233, 236, 239);
+    color: rgb(73, 80, 87);
+    &:hover {
+      background-color: rgb(233, 236, 239, 0.7);
+    }
+  }
+  & button.submit {
+    background-color: rgb(73, 80, 87);
+    &:hover {
+      background-color: green;
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 1rem 0;
+  background-color: white;
+  ${(props) => props.theme.border_box}
+`;
+
+const Line = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 0.66rem;
+  background: rgb(73, 80, 87);
+  height: 6px;
+  width: 4rem;
+  margin-top: 1.5rem;
+  margin-bottom: 1rem;
+  border-radius: 1px;
+`;
+const Title = styled.input`
+  font-size: 1.8rem;
+  outline: none;
+  border: none;
+  font-weight: bold;
+  width: 100%;
+  ${(props) => props.theme.border_box}
+`;
+
+const HashTagWrapper = styled.div`
+  ${(props) => props.theme.flex_row};
+  flex-wrap: wrap;
+  justify-content: flex;
+  align-items: center;
+  display: grid;
+`;
+
+const Input1 = styled.input`
+  outline: none;
+  border: none;
+  line-height: 1.5rem;
+  font-size: 1rem;
+`;
+
+const Input2 = styled.input`
+  outline: none;
+  border: none;
+  line-height: 1.5rem;
+  font-size: 1rem;
+  width: 50vw;
+  white-space: normal;
+`;
+
+const AddImage = styled.input`
+  cursor: pointer;
+  margin: 16px 0px;
+  display: flex;
+  width: 35vw;
+`;
+
+const AspectOutter = styled.div`
+  width: 50%;
+  min-width: 250px;
+`;
+const AspectInner = styled.div`
+  position: relative;
+  padding-top: 75%;
+  overflow: hidden;
+  background-image: url("${(props) => props.src}");
+  background-size: cover;
+`;
