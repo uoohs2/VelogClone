f96c583a7d67568e1cd77e794e48b891582387ad
Yoonji Lee <ji0ijs2@naver.com>
2022-04-15 16:56:24 +0900

    index

---
+++ src/components/core/index.js
@@ -0,0 +1,7 @@
+import Card from "./Card";
+import CardList from "./CardList";
+import CommentList from "./CommentList";
+import CommentWrite from "./CommentWrite";
+import Header from "./Header";
+
+export { Card, CardList, CommentList, CommentWrite, Header };
---
+++ src/components/ui/index.js
@@ -0,0 +1,8 @@
+import Button from "./Button";
+import Div from "./Div";
+import GlobalStyle from "./GlobalStyle";
+import Image from "./Image";
+import Input from "./Input";
+import Text from "./Text";
+
+export { Button, Div, GlobalStyle, Image, Input, Text };
