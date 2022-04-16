2216072d867d239c1a9fbc3636052951bf31cf47
Yoonji Lee <ji0ijs2@naver.com>
2022-04-16 11:48:08 +0900

    뷰 완성

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -8,16 +8,16 @@ const Login = (props) => {
       width="350px"
       height="700px"
       margin="70px auto"
+      padding="32px 0px"
       textCenter
-      border="1px solid lightgrey"
     >
-      <Div center height="550px" border="1px solid red">
-        <Div height="100px" border="1px solid lightgrey">
+      <Div center height="510px" margin="12px 0px" border="1px solid lightgrey">
+        <Div height="100px">
           <h1
             style={{
               fontSize: "2rem",
               lineHeigt: "55px",
-              margin: "10px 0px",
+              margin: "15px 0px",
             }}
           >
             Velog Login
@@ -31,28 +31,42 @@ const Login = (props) => {
             개발자를 위한 블로그 서비스에 가입해보세요.
           </h2>
         </Div>
-        <Div height="350px" border="1px solid black">
-          <hr style={{ margin: "10px 25px", border: "1px dotted lightgrey" }} />
-
-          <Div row margin="0px 0px 15px 0px" border="1px solid lightgrey">
-            <button
-              style={{
-                width: "300px",
-                lineHeight: "35px",
-                margin: "12px",
-                backgroundColor: "#96f2d7",
-              }}
+        <Div height="360px">
+          <Div row margin="0px 0px 15px 0px">
+            <Div
+              row
+              width="300px"
+              height="35px"
+              margin="20px 0px 10px 0px"
+              borderRadius="5px"
+              backgroundColor="#96f2d7"
             >
-              KakaoTalk으로 로그인
-            </button>
+              <Div inline width="20px" margin="0px 5px">
+                <img
+                  src="https://w.namu.la/s/059f8bf3e629d3f2e343fe3f3f10809022d58800962db675d233429660bf98d9ceccd60e23b1324d090c87485833b10c2c4503c93a230003ba67d5fcafa527930174f8daf70b9e9cb534ed3c2d096f775663373e7e848fad3f0e0445cf2a9e7e"
+                  alt="kakao"
+                  width="20px"
+                  height="20px"
+                />
+              </Div>
+              <button
+                style={{
+                  width: "150px",
+                  backgroundColor: "#96f2d7",
+                }}
+              >
+                KakaoTalk으로 로그인
+              </button>
+            </Div>
           </Div>
+          <hr style={{ margin: "10px 25px", border: "1px dotted lightgrey" }} />
           <input
             type="text"
             style={{
               width: "300px",
               height: "40px",
               lineHeight: "40px",
-              margin: "15px 0px 5px 0px",
+              margin: "10px 0px 5px 0px",
               border: "1px solid lightgrey",
             }}
             placeholder="아이디"
@@ -66,7 +80,7 @@ const Login = (props) => {
               margin: "5px 0px 5px 0px",
               border: "1px solid lightgrey",
             }}
-            placeholder="아이디"
+            placeholder="사용자 이름"
           />
           <input
             type="text"
@@ -77,7 +91,7 @@ const Login = (props) => {
               margin: "5px 0px 5px 0px",
               border: "1px solid lightgrey",
             }}
-            placeholder="아이디"
+            placeholder="비밀번호"
           />
           <input
             type="text"
@@ -88,7 +102,7 @@ const Login = (props) => {
               margin: "5px 0px",
               border: "1px solid lightgrey",
             }}
-            placeholder="비밀번호"
+            placeholder="비밀번호 확인"
           />
           <button
             style={{
