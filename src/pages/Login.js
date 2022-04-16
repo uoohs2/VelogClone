004368b9856faf7c379cd7bb6657a0287b2af920
Yoonji Lee <ji0ijs2@naver.com>
2022-04-16 10:52:20 +0900

    내용 서로 바뀐거 수정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,50 +2,32 @@ import React from "react";
 
 import { Div } from "../components/ui";
 
-const Login = (props) => {
+const Signup = (props) => {
   return (
     <Div
       width="350px"
-      height="700px"
+      height="680px"
       margin="70px auto"
+      padding="32px 0px"
       textCenter
-      border="1px solid lightgrey"
     >
-      <Div center height="550px" border="1px solid red">
-        <Div height="100px" border="1px solid lightgrey">
+      <Div
+        center
+        height="400px"
+        margin="12px 0px"
+        padding="10px 0px"
+        border="1px solid lightgrey"
+      >
+        <Div center height="100px">
           <h1
             style={{
               fontSize: "2rem",
-              lineHeigt: "55px",
-              margin: "10px 0px",
             }}
           >
-            Velog Login
+            Velog Signup
           </h1>
-          <h2
-            style={{
-              width: "270px",
-              fontSize: "1.2rem",
-            }}
-          >
-            개발자를 위한 블로그 서비스에 가입해보세요.
-          </h2>
         </Div>
-        <Div height="350px" border="1px solid black">
-          <hr style={{ margin: "10px 25px", border: "1px dotted lightgrey" }} />
-
-          <Div row margin="0px 0px 15px 0px" border="1px solid lightgrey">
-            <button
-              style={{
-                width: "300px",
-                lineHeight: "35px",
-                margin: "12px",
-                backgroundColor: "#96f2d7",
-              }}
-            >
-              KakaoTalk으로 로그인
-            </button>
-          </Div>
+        <Div height="300px">
           <input
             type="text"
             style={{
@@ -53,28 +35,6 @@ const Login = (props) => {
               height: "40px",
               lineHeight: "40px",
               margin: "15px 0px 5px 0px",
-              border: "1px solid lightgrey",
-            }}
-            placeholder="아이디"
-          />
-          <input
-            type="text"
-            style={{
-              width: "300px",
-              height: "40px",
-              lineHeight: "40px",
-              margin: "5px 0px 5px 0px",
-              border: "1px solid lightgrey",
-            }}
-            placeholder="아이디"
-          />
-          <input
-            type="text"
-            style={{
-              width: "300px",
-              height: "40px",
-              lineHeight: "40px",
-              margin: "5px 0px 5px 0px",
               border: "1px solid lightgrey",
             }}
             placeholder="아이디"
@@ -98,17 +58,29 @@ const Login = (props) => {
               backgroundColor: "#96f2d7",
             }}
           >
-            가입
+            로그인
           </button>
+          <hr style={{ margin: "10px 25px", border: "1px dotted lightgrey" }} />
+          <Div row margin="15px 0px">
+            <img
+              src="https://w.namu.la/s/059f8bf3e629d3f2e343fe3f3f10809022d58800962db675d233429660bf98d9ceccd60e23b1324d090c87485833b10c2c4503c93a230003ba67d5fcafa527930174f8daf70b9e9cb534ed3c2d096f775663373e7e848fad3f0e0445cf2a9e7e"
+              alt="kakao"
+              width="20px"
+              height="20px"
+            />
+            <p style={{ margin: "20px 10px", fontSize: "0.9em" }}>
+              KakaoTalk으로 로그인
+            </p>
+          </Div>
         </Div>
       </Div>
       <Div border="1px solid lightgrey" margin="12px 0px">
         <p style={{ display: "inline-block", fontSize: "0.9em" }}>
-          계정이 있으신가요?
+          계정이 없으신가요?
         </p>
         <button
           style={{
-            width: "50px",
+            width: "70px",
             height: "40px",
             lineHeight: "40px",
             margin: "10px 5px",
@@ -118,11 +90,11 @@ const Login = (props) => {
             color: "#39b7ab",
           }}
         >
-          로그인
+          가입하기
         </button>
       </Div>
     </Div>
   );
 };
 
-export default Login;
+export default Signup;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,32 +2,50 @@ import React from "react";
 
 import { Div } from "../components/ui";
 
-const Signup = (props) => {
+const Login = (props) => {
   return (
     <Div
       width="350px"
-      height="680px"
+      height="700px"
       margin="70px auto"
-      padding="32px 0px"
       textCenter
+      border="1px solid lightgrey"
     >
-      <Div
-        center
-        height="400px"
-        margin="12px 0px"
-        padding="10px 0px"
-        border="1px solid lightgrey"
-      >
-        <Div center height="100px">
+      <Div center height="550px" border="1px solid red">
+        <Div height="100px" border="1px solid lightgrey">
           <h1
             style={{
               fontSize: "2rem",
+              lineHeigt: "55px",
+              margin: "10px 0px",
             }}
           >
-            Velog Signup
+            Velog Login
           </h1>
+          <h2
+            style={{
+              width: "270px",
+              fontSize: "1.2rem",
+            }}
+          >
+            개발자를 위한 블로그 서비스에 가입해보세요.
+          </h2>
         </Div>
-        <Div height="300px">
+        <Div height="350px" border="1px solid black">
+          <hr style={{ margin: "10px 25px", border: "1px dotted lightgrey" }} />
+
+          <Div row margin="0px 0px 15px 0px" border="1px solid lightgrey">
+            <button
+              style={{
+                width: "300px",
+                lineHeight: "35px",
+                margin: "12px",
+                backgroundColor: "#96f2d7",
+              }}
+            >
+              KakaoTalk으로 로그인
+            </button>
+          </Div>
           <input
             type="text"
             style={{
@@ -35,6 +53,28 @@ const Signup = (props) => {
               height: "40px",
               lineHeight: "40px",
               margin: "15px 0px 5px 0px",
+              border: "1px solid lightgrey",
+            }}
+            placeholder="아이디"
+          />
+          <input
+            type="text"
+            style={{
+              width: "300px",
+              height: "40px",
+              lineHeight: "40px",
+              margin: "5px 0px 5px 0px",
+              border: "1px solid lightgrey",
+            }}
+            placeholder="아이디"
+          />
+          <input
+            type="text"
+            style={{
+              width: "300px",
+              height: "40px",
+              lineHeight: "40px",
+              margin: "5px 0px 5px 0px",
               border: "1px solid lightgrey",
             }}
             placeholder="아이디"
@@ -58,29 +98,17 @@ const Signup = (props) => {
               backgroundColor: "#96f2d7",
             }}
           >
-            로그인
+            가입
           </button>
-          <hr style={{ margin: "10px 25px", border: "1px dotted lightgrey" }} />
-          <Div row margin="15px 0px">
-            <img
-              src="https://w.namu.la/s/059f8bf3e629d3f2e343fe3f3f10809022d58800962db675d233429660bf98d9ceccd60e23b1324d090c87485833b10c2c4503c93a230003ba67d5fcafa527930174f8daf70b9e9cb534ed3c2d096f775663373e7e848fad3f0e0445cf2a9e7e"
-              alt="kakao"
-              width="20px"
-              height="20px"
-            />
-            <p style={{ margin: "20px 10px", fontSize: "0.9em" }}>
-              KakaoTalk으로 로그인
-            </p>
-          </Div>
         </Div>
       </Div>
       <Div border="1px solid lightgrey" margin="12px 0px">
         <p style={{ display: "inline-block", fontSize: "0.9em" }}>
-          계정이 없으신가요?
+          계정이 있으신가요?
         </p>
         <button
           style={{
-            width: "70px",
+            width: "50px",
             height: "40px",
             lineHeight: "40px",
             margin: "10px 5px",
@@ -90,11 +118,11 @@ const Signup = (props) => {
             color: "#39b7ab",
           }}
         >
-          가입하기
+          로그인
         </button>
       </Div>
     </Div>
   );
 };
 
-export default Signup;
+export default Login;
