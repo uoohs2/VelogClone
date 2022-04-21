b6e0353de7db5baf047581456b40e17fcde4ef30
이성영 <syounglee012@gmail.com>
2022-04-21 03:24:27 -0700

    gg

--- src/components/core/index.js
+++ src/components/core/index.js
@@ -1,9 +1,8 @@
 import Card from "./Card";
-import CardList from "./CardList";
 import CommentList from "./CommentList";
 import CommentWrite from "./CommentWrite";
 import DetailUser from "./DetailUser";
 import Edit from "./Edit";
 import Header from "./Header";
 
-export { Card, CardList, CommentList, CommentWrite, DetailUser, Edit, Header };
+export { Card, CommentList, CommentWrite, DetailUser, Edit, Header };
